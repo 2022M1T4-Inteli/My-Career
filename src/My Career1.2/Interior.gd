581d070b80efa419efa1d636cf9a518bba0f51c0
lfggranville <58540177+lfggranville@users.noreply.github.com>
2022-02-28 17:47:22 -0300

    Erros

--- src/My Career1.2/Interior.gd
+++ src/My Career1.2/Interior.gd
@@ -6,22 +6,19 @@ extends Sprite
 # var b = "text"
 onready var Legenda = get_tree().get_root().get_node("Level 2").get_node("Player").get_node("Camera2D").get_node("InstruçãoComando")
 onready var Texto =get_tree().get_root().get_node("Level 2").get_node("Player").get_node("Camera2D").get_node("InstruçãoComando").get_node("Legenda")
-# Called when the node enters the scene tree for the first time.
+# Criando as variáveis Legenda e Texto para que seja possível puxar valores dos repectivos objetos de Player
 func _ready():
-	print(Legenda)
-
-
-
+	pass
 
 func _on_AreaInteraoMentor_body_entered(body):
 	print(body.get_name())
 	if body.get_name() == "Player":
-		Legenda.show() 
+			Legenda.show() 
 		
-	pass # Replace with function body.
+	pass # Essa função diz que quando a área 2D for preenchida pelo objeto Player então a Variável legenda deve ser visível
 
 
 func _on_AreaInteraoMentor_body_exited(body):
 	if body.get_name() == "Player":
 		Legenda.hide() 
-	pass # Replace with function body.
+	pass # Essa função diz que quando a área 2D não for preenchida pelo objeto Player então a Variável legenda deve ser oculta
