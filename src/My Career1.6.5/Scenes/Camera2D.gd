0390a3f8553dec78ab2437e148e10608df1c5447
lfggranville <58540177+lfggranville@users.noreply.github.com>
2022-03-16 13:57:07 -0300

    Atualização 1.6.5

--- src/My Career1.6.5/Scenes/Camera2D.gd
+++ src/My Career1.6.5/Scenes/Camera2D.gd
@@ -5,9 +5,12 @@ onready var PlayerName = get_node("/root/OpenWorld/Player/CaixaNome/TextEdit")
 
 func _ready():
 	pass 
+	
+func _process(delta):
+	if Input.is_action_pressed("ui_enter"):
+		_on_Button2_pressed()
 
 func _getcharactersName():
-	while PlayerName.text != "":
 		Box.visible = true
 
 func _on_Button_pressed():
@@ -19,10 +22,16 @@ func _on_Button_pressed():
 
 func dialog_listener(string):
 	match string:
-		"mentorend0":
+		"godend0":
 			GameManager.goddialogcount += 1
 #Nessa função caso uma das strings seja detectada então é executada uma série de instruções para mudança de dialogo e outros.
 
 
-func _on_TextEdit_text_changed():
-	Dialogic.set_variable("playername",PlayerName.text)
+func _on_Button2_pressed():
+		Dialogic.set_variable("playername",PlayerName.text)
+		Box.visible = false
+		if get_node_or_null('DialogNode') == null: #Verifica se outro diálogo ja esta em cena
+			var dialog = Dialogic.start(str("AlanTuring-",GameManager.goddialogcount)) #Inicia o diálogo da timeline-1, que foi pré-definida na ferramente "Dialogic"
+			dialog.connect("dialogic_signal", self, "dialog_listener") #Faz com que o sinal de dialogo receba um valor e assim execute uma ação
+			# Declarando variaveis de pontuação dos bachalerados tanto de experiência quanto de quiz
+			add_child(dialog)
