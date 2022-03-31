ccb1089e44d8596315a4ee08fca6f12d1993f8ca
KaiqueRamon <kaique.dantas@sou.inteli.edu.br>
2022-03-31 18:28:36 -0300

    alteração no tamanho do mini game da nasa

---
+++ src/My Career1.8.1/Scenes/OpenWorld.gd
@@ -0,0 +1,48 @@
+extends Node2D
+
+func _ready():
+	if GameManager.tutorial == false:
+		get_node("Player").get_node("Camera2D")._on_Button_pressed()
+		GameManager.tutorial = true
+	else:
+		pass
+
+
+func _on_ElonMusgo_body_entered(body):
+	get_tree().change_scene("res://Scenes/ElonMusgo.tscn")
+# Quando a Area2D detecta o valor de "body" então a cena é mudada para o interior do prédio do mentor Elon Musgo.
+
+
+func _on_Tesla_body_entered(body):
+	get_tree().change_scene("res://Scenes/TerreoTesla.tscn")
+# Quando a Area2D detecta o valor de "body" então a cena é mudada para o interior do prédio do mentor Elon Musgo.
+
+
+func _on_SteveTrabalho_body_entered(body):
+	get_tree().change_scene("res://Scenes/SteveTrabalho.tscn")
+# Quando a Area2D detecta o valor de "body" então a cena é mudada para o interior do prédio da Tesla.
+
+
+func _on_DorothyVan_body_entered(body):
+	get_tree().change_scene("res://Scenes/DorothyVan.tscn")
+	
+
+func _on_BillPortas_body_entered(body):
+	get_tree().change_scene("res://Scenes/BillPortas.tscn")
+
+
+
+func _on_Apple_body_entered(body):
+	get_tree().change_scene("res://Scenes/TerreoApple.tscn")
+
+
+func _on_Microsoft_body_entered(body):
+	get_tree().change_scene("res://Scenes/TerreoMicrosoft.tscn")
+
+
+func _on_Nasa_body_entered(body):
+	get_tree().change_scene("res://Scenes/TerreoNasa.tscn")
+
+
+func _on_Biblioteca_body_entered(body):
+	get_tree().change_scene("res://Scenes/Node2D.tscn")
