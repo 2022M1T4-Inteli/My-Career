219b963f90b559f3a87ef9fa9fcafe8d484cfb9e
GabrielNhoncanse <gabriel.nhoncanse@sou.inteli.edu.br>
2022-03-31 14:48:29 -0300

    Versão 1.8.2

---
+++ src/My Career1.8.2/Scenes/GameManager.gd
@@ -0,0 +1,33 @@
+extends Node
+
+var tsdialogcount = 0 #terreo tesla
+var emdialogcount = 0 #elon musgo
+var stdialogcount = 0 #steve trab
+var dvdialogcount = 0 #dorothy van
+var bpdialogcount = 0 #bill portas
+var rtdialogcount = 0 #recrutadora tesla
+var radialogcount = 0 #recrutadora apple
+var apdialogcount = 0 #terreo apple
+var msdialogcount = 0 #terreo microsoft
+var rmdialogcount = 0 #recrutadora microsoft
+var nsdialogcount = 0 #terreo nasa
+var rndialogcount = 0 #recrutadora nasa
+var goddialogcount = 0 #god
+
+var pcdialogcount = 0 #placa
+
+var ccxppoints = 0
+
+var iniciominigamems = true
+var iniciominigameap = true
+var inicioms = true
+var inicioap = true
+var iniciona = true
+var iniciots = true
+
+var velocidadevertical = 500
+var velocidadehorizontal = 500
+#var velocidadeverticalmp = 15.3
+#var velocidadehorizontalmp = 15.3
+
+var tutorial = false
