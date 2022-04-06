7006a36b594115326fb3bb1d8d3cc68164b937c3
KaiqueRamon <kaique.dantas@sou.inteli.edu.br>
2022-04-06 01:40:10 -0300

    1.9.4

---
+++ src/My Career 1.9.4/Scenes/Area2DApple.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+func _ElevadorEnable():
+	$CollisionShape2D.disabled = false
+
+func _ready():
+	if GameManager.firsttimeap == 0:
+		$CollisionShape2D.disabled = true
+	else: 
+		$CollisionShape2D.disabled = false
