a5e0e2e1ffb53eb1fed5ef755c69f1d01c80b256
luisaleite <99203030+luisaleite@users.noreply.github.com>
2022-04-08 16:48:36 -0300

    Atualização 1.9.8
    Atualizado por por @lfggranville @luisaleite

---
+++ src/My Career 1.9.8/My Career 1.9.8/Scenes/Node2D.gd
@@ -0,0 +1,64 @@
+extends Node2D
+
+func _ready():
+	if GameManager.ajudabiblioteca == false:
+		get_node("Bibliotecario2/Bibliotecário1/Player").global_position = GameManager.positionbiblioteca
+
+func _on_Button_pressed():
+	get_tree().change_scene("res://Scenes/SalaSI.tscn")
+	pass # Replace with function body.
+
+
+func _on_Button2_pressed():
+	get_tree().change_scene("res://Scenes/SalaCC.tscn")
+	pass # Replace with function body.
+
+
+func _on_Button3_pressed():
+	get_tree().change_scene("res://Scenes/SalaEC.tscn")
+	pass # Replace with function body.
+
+
+func _on_Button4_pressed():
+	get_tree().change_scene("res://Scenes/SalaES.tscn")
+	pass # Replace with function body.
+
+
+func _on_Button1_pressed():
+	OS.shell_open("https://www.instagram.com/acientistadedados/?utm_medium=copy_link")
+	pass # Replace with function body.
+
+
+func _on_ButtonComputer_pressed():
+	get_tree().change_scene("res://Scenes/Computer.tscn")
+	pass # Replace with function body.
+
+
+
+func _on_ButtonCarreiraSI_pressed():
+	OS.shell_open("https://www.youtube.com/watch?v=cPLJ-hV1oxU")
+	pass # Replace with function body.
+
+
+func _on_ButtonAreaTrab_pressed():
+	OS.shell_open("https://www.infojobs.com.br/vagas-de-emprego-")
+	pass # Replace with function body.
+
+
+func _on_ButtonVoltarSI_pressed():
+	get_tree().change_scene("res://Scenes/Biblioteca.tscn")
+	pass # Replace with function body.
+
+
+func _on_ButtonUnivSI_pressed():
+	OS.shell_open("https://www.inteli.edu.br/sistemas-de-informacao/")
+	pass # Replace with function body.
+
+
+func _on_Area2D_body_entered(body):
+	get_tree().change_scene("res://Scenes/OpenWorld.tscn")
+
+
+func _on_Button5_pressed():
+	get_tree().change_scene("res://Scenes/TelaRanking.tscn")
+	pass # Replace with function body.
---
+++ src/My Career 1.9.8/My Career 1.9.8/Scenes/SalaES.gd
@@ -0,0 +1,42 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	GameManager.ajudabiblioteca = false
+	get_tree().paused = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_ButtonES_pressed():
+	OS.shell_open("https://instagram.com/dafcoding")
+	pass # Replace with function body.
+
+
+func _on_ButtonAreatrabES_pressed():
+	OS.shell_open("https://www.napratica.org.br/profissao-engenheiro-de-")
+	pass # Replace with function body.
+
+
+func _on_ButtonCarreiraES_pressed():
+	OS.shell_open("https://www.undb.edu.br/blog/as-4-melhores-areas-de-atuacao-do-engenheiro-de-software")
+	pass # Replace with function body.
+
+
+func _on_ButtonVoltarES_pressed():
+	get_tree().change_scene("res://Scenes/Biblioteca.tscn")
+	pass # Replace with function body.
+
+
+func _on_ButtonUnivES_pressed():
+	OS.shell_open("https://www.inteli.edu.br/engenharia-de-software/")
+	pass # Replace with function body.
