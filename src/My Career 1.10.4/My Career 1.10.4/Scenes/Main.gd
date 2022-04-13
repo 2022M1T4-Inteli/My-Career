36c0e1a38d19e364232c37d47877519325dda618
luisaleite <99203030+luisaleite@users.noreply.github.com>
2022-04-12 21:28:07 -0300

    atualização e exclui a sessão 1.10.4

--- src/My Career 1.10.4/My Career 1.10.4/Scenes/Main.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-func _ready():
-	pass
-
-func _on_Wire_Task_Button_pressed():
-	get_tree().change_scene("res://WireTask.tscn")
-#função que move o jogador pra cena do wire task
--- src/My Career 1.10.4/My Career 1.10.4/Scenes/RoboPixelPng.gd
+++
@@ -1,9 +0,0 @@
-extends Sprite
-
-func _ready():
-	pass
-
-func _process(delta):
-	if ScoreLabel.score == 4:
-		visible = true
-#faz visil do robo ligado depois que a variavel global fica igual a 4
